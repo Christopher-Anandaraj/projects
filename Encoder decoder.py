a4d43ed57c519c2d8a28284047c4ab2bd5f5d581
Chris <61247123+Christopher-Anandaraj@users.noreply.github.com>
2023-10-26 22:59:37 -0400

    Update Encoder decoder.py

--- Encoder decoder.py
+++ Encoder decoder.py
@@ -8,46 +8,54 @@ while True:
 
         while True:
             try:
-                scale = int(input("Enter your scale of encryption (1-256): "))
-                if 1 <= scale <= 256:
-                    break  # Break the inner loop if the input is a valid number within the specified range
+                shift = int(input("Enter your shift value (1-25): "))
+                if 1 <= shift <= 25:
+                    break  
                 else:
-                    print("Scale must be between 1 and 256.")
+                    print("Shift value must be between 1 and 25.")
             except ValueError:
                 print("Input a valid number.")
 
-        # Encrypt the input text using ASCII index and scale
+        
         encrypted_text = ""
         for char in enc_text:
-            encrypted_char = chr((ord(char) + scale) % 256)  # Encrypt using ASCII and wrap around if necessary
-            encrypted_text += encrypted_char
+            if char.isalpha():
+                shifted_char = chr((ord(char) - ord('a' if char.islower() else 'A') + shift) % 26 + ord('a' if char.islower() else 'A'))
+                encrypted_text += shifted_char
+            else:
+                encrypted_text += char
 
         print("Encrypted text:", encrypted_text)
-        
-        # Copy the encrypted text to clipboard
+
+       
         pyperclip.copy(encrypted_text)
-        print("Encrypted text copied to clipboard.")
+        print("Encrypted text copied to clipboard.\n")
 
     elif purpose == "decode" or purpose == "d":
         dec_text = input("Enter text to decode: ")
 
         while True:
             try:
-                scale = int(input("Enter your scale of decryption (1-256): "))
-                if 1 <= scale <= 256:
-                    break  # Break the inner loop if the input is a valid number within the specified range
+                shift = int(input("Enter your shift value (1-25): "))
+                if 1 <= shift <= 25:
+                    break  
                 else:
-                    print("Scale must be between 1 and 256.")
+                    print("Shift value must be between 1 and 25.")
             except ValueError:
                 print("Input a valid number.")
 
         
         decrypted_text = ""
         for char in dec_text:
-            decrypted_char = chr((ord(char) - scale) % 256)  
-            decrypted_text += decrypted_char
+            if char.isalpha():
+                shifted_char = chr((ord(char) - ord('a' if char.islower() else 'A') - shift) % 26 + ord('a' if char.islower() else 'A'))
+                decrypted_text += shifted_char
+            else:
+                decrypted_text += char
 
-        print("Decrypted text:", decrypted_text)
+        print("Decrypted text:", decrypted_text, "\n")
 
+    elif purpose == "kill":
+        break
     else:
-        print("Invalid input. Please enter 'encode' or 'decode'.")
+        print("Invalid input. Please enter 'encode' or 'decode'.\n")
